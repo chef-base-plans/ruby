934d7faf67c89e59f1294c91e02e386f89f08582
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-04-08 14:04:53 +0530

    Bumped 3.0.1 version

--- plan.sh
+++ plan.sh
@@ -1,14 +1,14 @@
 pkg_name=ruby
 pkg_origin=core
-pkg_version=2.7.2
+pkg_version=3.0.1
 pkg_description="A dynamic, open source programming language with a focus on \
   simplicity and productivity. It has an elegant syntax that is natural to \
   read and easy to write."
 pkg_license=("Ruby")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=https://cache.ruby-lang.org/pub/${pkg_name}/2.7/${pkg_name}-${pkg_version}.tar.gz
+pkg_source=https://cache.ruby-lang.org/pub/${pkg_name}/3.0/${pkg_name}-${pkg_version}.tar.gz
 pkg_upstream_url=https://www.ruby-lang.org/en/
-pkg_shasum=6e5706d0d4ee4e1e2f883db9d768586b4d06567debea353c796ec45e8321c3d4
+pkg_shasum=369825db2199f6aeef16b408df6a04ebaddb664fb9af0ec8c686b0ce7ab77727
 pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi core/readline core/nss-myhostname)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
 pkg_lib_dirs=(lib)
@@ -38,6 +38,6 @@ do_check() {
 
 do_install() {
   do_default_install
-  gem update --system 2.7.9 --no-document
+  gem update --system 3.2.3 --no-document
   gem install rb-readline --no-document
 }
